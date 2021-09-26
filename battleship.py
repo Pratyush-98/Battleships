77510eb08d3d352299a8c59cfc5cb78af3184ce0
Pratyush-98 <pratyushsharma899@msitprogram.net>
2021-09-26 19:37:53 +0530

    completed creating emptygrid

--- battleship.py
+++ battleship.py
@@ -62,8 +62,14 @@ Parameters: int ; int
 Returns: 2D list of ints
 '''
 def emptyGrid(rows, cols):
-    grid=[[1]*cols]*rows
+    grid=[]
+    for i in range(rows):
+        a=[]
+        for j in range(cols):
+            a.append(1)
+        grid.append(a)
     return grid
+
 
 
 '''
@@ -272,4 +278,4 @@ if __name__ == "__main__":
 
     ## Finally, run the simulation to test it manually ##
     #runSimulation(500, 500)
-  test.emptyGrid(10,10)
+  test.testEmptyGrid()
